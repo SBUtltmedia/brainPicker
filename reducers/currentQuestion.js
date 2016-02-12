4d23d0cd779ff671d51c80849a87155602292324
Ken Fehling <kenneth.fehling@stonybrook.edu>
2016-02-12 17:28:19 -0500

    Fixed "hit" event on polygon

--- reducers/currentQuestion.js
+++ reducers/currentQuestion.js
@@ -1,14 +1,14 @@
 import * as types from '../constants/ActionTypes';
-
+const DEFAULT_LAYER = '1';
 const initialState={
   region: '',
   points: [],
-  layer: '1'
+  layer: DEFAULT_LAYER
 };
 export default function structures(state = initialState, action) {
   switch (action.type) {
     case types.SHOW_QUESTION:
-      return Object.assign({}, {layer: state.layer}, action.question);
+      return Object.assign({}, {layer: DEFAULT_LAYER}, action.question);
     case types.CHANGE_LAYER:
       return Object.assign({}, state, {layer: action.layer});
     default:
