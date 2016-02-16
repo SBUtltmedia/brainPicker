3ec2b96e6476e26968388db2825d48669c9175f4
TLT Media Lab <danpark@dh017-169.medilab.tlt.stonybrook.edu>
2016-02-16 18:56:10 -0500

    Slider for Layer
    add css
    add slider for layers

--- components/Region.js
+++ components/Region.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 const style = {
    fill: 'blue',
    stroke: "green",
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const store = configureStore();
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <App  />
   </Provider>,
   document.getElementById('root')
 );
