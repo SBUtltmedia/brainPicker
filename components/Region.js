799a8d5e571fe8cd2f0f7a6c9d1bcc0a0f16d330
TLT Media Lab <pstdenis@dh017-169.medilab.tlt.stonybrook.edu>
2016-02-16 14:28:12 -0500

    map over regions in region.js
    Hack because component loads once with undefined
    “currentQuestion.points[currentQuestion.layer]” how to fix?

--- components/Region.js
+++ components/Region.js
@@ -8,9 +8,25 @@ const style = {
    strokeWidth: "10px"
  };
 
-export default ({actions, currentQuestion}) => (
+export default ({actions, currentQuestion}) => {
+if(currentQuestion.points[currentQuestion.layer]){
+return(
   <svg style={style} width={500} height={500}>
+  {currentQuestion.points[currentQuestion.layer].map((contiguousPoints,i) =>
     <polygon onClick={() => console.log('hit')}
-    points={currentQuestion.points[currentQuestion.layer]}/>
+    points={contiguousPoints}/>)}
   </svg>
-);
+)
+}
+else {
+  return(
+    <svg style={style} width={500} height={500}>
+
+    </svg>
+  )
+
+
+}
+
+}
+;
