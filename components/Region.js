76e2dbabfbd876b58e48af0fca71383b03c4ea09
danbee207 <danpark@dh017-167.medilab.tlt.stonybrook.edu>
2016-02-29 14:15:35 -0500

    - IMAGE & REGION OVERLAY - div postion

--- components/Region.js
+++ components/Region.js
@@ -1,19 +1,48 @@
+import Catscan from '../components/Catscan';
+
 const style = {
    fill: 'blue',
    stroke: "green",
    fillOpacity: 0.5,
    strokeOpacity: 0.5,
-   strokeWidth: "10px"
+   strokeWidth: "10px",
+  margin : 0 ,
+  padding: 0,
+   width:500,
+   height : 500,
+   position : 'absolute',
+   left : 0,
+   top : 0,
+ };
+
+ const styleCatscat = {
+   margin : 0 ,
+   padding: 0,
+   position : 'absolute',
+   left: 0,
+   width:'inherit',
+   height : 'inherit',
+   top : 0
+
  };
 
 export default ({actions, currentQuestion}) => {
 if(currentQuestion.points[currentQuestion.layer]){
+
 return(
-  <svg style={style} width={500} height={500}>
+  <div style={style}>
+
+  <Catscan currentQuestion={currentQuestion}  style={styleCatscat}/>
+  <svg style={style} >
+
+
   {currentQuestion.points[currentQuestion.layer].map((contiguousPoints,i) =>
-    <polygon onClick={() => console.log('hit')}
+    <polygon key={i} onClick={() => console.log('hit')}
     points={contiguousPoints}/>)}
+
   </svg>
+
+</div>
 )
 }
 else {
