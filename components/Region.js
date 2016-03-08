fdb132b57507692aa7002925f90d0e43fe9dd289
Kenneth T Fehling <kfehling@dh017-169.medilab.tlt.stonybrook.edu>
2016-03-08 17:34:18 -0500

    Refactored, added CLEAR_MARKERS

--- components/Region.js
+++ components/Region.js
@@ -18,23 +18,15 @@ export default ({actions, currentQuestion}) => {
   const src = "data/images/catscan/"+(currentQuestion.layer)+".png";
   const points = currentQuestion.points[currentQuestion.layer] || [];
   const markers = currentQuestion.markers[currentQuestion.layer - 1] || [];
-
-  console.log("markers", markers);
-
   return <div>
     <svg id="brainImage" xmlns="http://www.w3.org/2000/svg" style={styleSVG} viewBox="0 0 500 500" >
-      <image x="0" y="0" width="100%" height="100%" xlinkHref={src}
-     onClick={actions.putMarker}/>
+      <image x="0" y="0" width="100%" height="100%" xlinkHref={src} onClick={actions.putMarker}/>
 
       {points.map((contiguousPoints, i) =>
-        <polygon key={i} onClick={() => console.log('hit')}
-        points={contiguousPoints}/>)}
+        <polygon key={i} onClick={() => console.log('hit')} points={contiguousPoints}/>)}
 
       {markers.map((marker, i) =>
-        <Marker color={"#93268f"} position={marker.position} key={i}
-        onClick={() => actions.removeMarker(currentQuestion.layer, i)} />)}
+        <Marker color={"#93268f"} position={marker.position} key={i} onClick={() => actions.removeMarker(i)} />)}
     </svg>
-
-
   </div>;
 }
