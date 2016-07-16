08a92054abf634fc25ff2e9e2615c0ece1d36e02
Kevin A Barry <Kevin.A.Barry@stonybrook.edu>
2016-07-15 21:27:45 -0400

    Added buttons for each layer to replace the functionality that we had in the last app where the user could see if they placed a point on a layer regardless of what layer they were on. THESE STILL NEED TO BE STYLED TO APPEAR SMALLER / NEXT TO EACHOTHER / IN LINE WITH THE IMAGE

--- components/LayerChanger.js
+++ components/LayerChanger.js
@@ -1,15 +1,18 @@
 import ReactSlider from 'react-slider';
 import './LayerChanger.scss';
-
+import PointDisplayBank from './PointDisplayBank';
 const styleRange = {
 
  };
 
 export default ({actions, currentQuestion,layer,max }) => {
 
-	return <div>
-		<input style={styleRange} className="layerPicker" type="range" value={layer} min="1" max={max-1}
-		orient="vertical" onChange={value => {actions.changeLayer(value.target.value)}}/>
-	</div>
+console.log(actions, currentQuestion,layer,max)
 
+	return (<div>
+		<input style={styleRange} className="layerPicker" type="range" value={layer} min="1" max={max-1} orient="vertical" onChange={value => {actions.changeLayer(value.target.value)}}/>
+
+  <PointDisplayBank  min="1" max={max-1}/>
+  </div>
+)
 };
---
+++ components/PointDisplayBank.js
@@ -0,0 +1,38 @@
+
+import { connect } from 'react-redux';
+import {Colors, Button} from 'react-foundation';
+import {changeLayer} from '../actions/actions';
+import PointDisplay from './PointDisplay';
+const mapStateToProps = (state, ownProps) => {
+  return {
+		markers: state.currentQuestion.markers,
+    pointsPerLayer:state.currentQuestion.pointsPerLayer
+  };
+}
+const mapDispatchToProps = (dispatch, ownProps, state) => ({
+
+});
+
+
+
+
+
+const styleRange = {
+
+ };
+
+
+
+
+export default ({max}) => {
+console.log(max)
+
+	return <div>
+
+
+	{ Array(max).fill(0).map((nothing, i) =>	<PointDisplay layer={i}/>)}
+	</div>
+
+
+
+};
