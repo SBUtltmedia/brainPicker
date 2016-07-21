21901822c3bc94a8ddf9224cb1276872c3c2d152
Kevin A Barry <Kevin.A.Barry@stonybrook.edu>
2016-07-21 19:32:14 -0400

    The array of layer selecting buttons now affects the state tree in the same way that the slider did (ownProps, layer)! In fact we were joking around that we could even replace the slider in it's entirety with just the buttons, they still need to be stylized via Foundation to fit neatly into that column. We got the slider stylized a little bit, but it's BROKEN in Firefox now.

--- components/PointDisplay.js
+++ components/PointDisplay.js
@@ -1,23 +1,30 @@
 import { connect } from 'react-redux';
 import {Colors, Button} from 'react-foundation';
+import {changeLayer} from '../actions/actions';
 
 const mapStateToProps = (state, ownProps) => {
   return {
 		markers: state.currentQuestion.markers,
-    pointsPerLayer:state.currentQuestion.pointsPerLayer
+    pointsPerLayer:state.currentQuestion.pointsPerLayer,
+    layerNum:state.currentQuestion.layer
   };
 }
 const mapDispatchToProps = (dispatch, ownProps, state) => ({
-	showQuestion: () => dispatch(showQuestion(ownProps.question))
+
+  		changeLayer: () => {
+        console.log(ownProps)
+        dispatch(changeLayer(ownProps.layer+1))
+      }
+
 });
 
 
-const PointDisplay = ({markers,pointsPerLayer}) =>{
+const PointDisplay = ({markers,pointsPerLayer,layerNum,changeLayer}) =>{
 const buttonColor = Colors.ALERT;
 console.log(pointsPerLayer)
  return (
    <div>
-{ Array(pointsPerLayer).fill(0).map((nothing, i) =>	<Button color={buttonColor} buttonNumber={i} >{i}</Button>)}
+{ Array(pointsPerLayer).fill(0).map((nothing, i) =>	<Button color={buttonColor} key={i}  buttonNumber={i}   onClick={changeLayer}></Button>)}
 </div>)
 
 }
