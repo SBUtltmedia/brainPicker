d32bc26f0e2ea6a5dd6c8c661931b508b9dccdd2
Kevin A Barry <Kevin.A.Barry@stonybrook.edu>
2016-07-29 19:05:24 -0400

    BUMP ON THE STYLE!

--- components/PointDisplayBank.js
+++ components/PointDisplayBank.js
@@ -6,7 +6,7 @@ import PointDisplay from './PointDisplay';
 const mapStateToProps = (state, ownProps) => {
   return {
 		markers: state.currentQuestion.markers,
-    pointsPerLayer:state.currentQuestion.pointsPerLayer
+    pointsPerLayer:state.currentQuestion.pointsPerLayer,
   };
 }
 const mapDispatchToProps = (dispatch, ownProps, state) => ({
@@ -15,13 +15,13 @@ const mapDispatchToProps = (dispatch, ownProps, state) => ({
 
 
 
-export default ({max}) => {
+export default ({max,currentLayer}) => {
 console.log(max)
 
 	return <div>
 
 
-	{ Array(max).fill(0).map((nothing, i) =>	<PointDisplay key={i} layer={max - 1 - i}/>)}
+	{ Array(max).fill(0).map((nothing, i) =>	<PointDisplay   key={i} layer={max - 1 - i}/>)}
 	</div>
 
 
