41b70d277044b0bc18759ce64a4498d3fb2eec7d
Joseph A Wilson <joawilson@gonzo-tll.labs.stonybrook.edu>
2018-07-24 16:45:32 -0400

    Updated version July/24/18

--- README.md
+++
@@ -1,8 +0,0 @@
-## Setup
-`npm install`
-
-## Running in development
-`npm start`
-
-## Running tests
-`mocha --compilers js:babel-core/register --watch tests/**/*.test.js`
--- constants/ActionTypes.js
+++
@@ -1,8 +0,0 @@
-export const SHOW_QUESTION = 'show-question';
-export const LOAD_QUESTION_DOTS = 'd-questiondot';
-export const CHANGE_LAYER = 'change-layer';
-export const WHEEL_CHANGE = 'wheel-change';
-export const ADD_MARKER = 'add-marker';
-export const REMOVE_MARKER = 'remove-marker';
-export const CLEAR_MARKERS = 'clear-markers';
-export const SUBMIT_ANSWERS = 'submit-answers';
